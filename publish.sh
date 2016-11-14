ae3fc8ccab0d4bde1070cb03292b310c9b606859
Hannah Warmbier <hannahwarmbier@gmail.com>
2016-11-14 13:07:10 -0600

    Add publish script
    *crosses fingers*

---
+++ publish.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: ./bin/publish \"commit message\""
+    exit 1;
+fi
+
+if git diff-index --quiet HEAD --; then
+    echo "No changes to commit."
+    exit 1;
+fi
+
+git checkout develop
+sculpin generate --env=prod
+
+git add *
+git commit -m "$1"
+git push origin `git subtree split --prefix output_prod develop`:master --force
