d8d7789b57b7e0bd4a039441b4405e7016aa5309
Hannah Warmbier <hannahwarmbier@gmail.com>
2017-02-15 15:46:42 -0600

    Merge branch feature/netlify
    - Adds .nvmrc, .travis.yml
    - Updates .gitignore, publish.sh
    - Temporary change to sculpin_site.yml of site URL to Netlify subdomain

--- publish.sh
+++ publish.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 1 ]; then
-  echo "Usage: ./bin/publish \"commit message\""
-  exit 1;
-fi
+[ -z "$NETLIFY_ID" ] && echo "Need to pass Netlify site ID as: NETLIFY_ID" && exit 1;
+[ -z "$NETLIFY_TOKEN" ] && echo "Need to pass Netlify access token as: NETLIFY_TOKEN" && exit 1;
 
-if git diff-index --quiet HEAD --; then
-  echo "No changes to commit."
-  exit 1;
-fi
-
-git checkout develop
-./vendor/bin/sculpin generate --env=prod
-
-git add *
-git commit -m "$1"
-git push origin `git subtree split --prefix output_prod develop`:master --force
+netlify deploy --site-id="$NETLIFY_ID" --api-token="$NETLIFY_TOKEN"
